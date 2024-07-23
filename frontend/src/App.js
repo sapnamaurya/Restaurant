868006eab2e91297565617e926f964a2bb1f5dd6
sapna maurya <io5.sapnamaurya@gmail.com>
2024-07-23 20:45:00 +0530

    pro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Story from "./Recipe/Story";
+import Updates from "./Recipe/Updates";
+import Menu from "./Recipe/Menu";
+import Contact from "./Recipe/Contact";
+import Home from "./Recipe/Home";
+import Nav from "./Recipe/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Reserve from "./Recipe/Reserve";
+import Signup from "./Recipe/Signup";
+import Login from "./Recipe/Login";
+import Comments from "./Recipe/Comments";
+import UpdateComment from "./Recipe/UpdateComment";
+import CommentBox from "./Recipe/CommentBox";
+import NewsLetter from "./Recipe/NewsLetter";
+/* import Protected from './Recipe/Protected'; */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <BrowserRouter>
+          <Nav></Nav>
+          <Routes>
+            <Route path="/story" element={<Story />} />
+            <Route path="/updates" element={<Updates />} />
+            <Route path="/update/:id" element={<UpdateComment />} />
+
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/comments" element={<Comments></Comments>} />
+            <Route path="/commentbox" element={<CommentBox></CommentBox>} />
+
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/news" element={<NewsLetter />} />
+
+            <Route path="/reserve" element={<Reserve />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
